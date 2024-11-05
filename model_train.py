eac9edde62e172b25bf03226599bd41d344dcc9a
Ting Ting Chen Zeng <tin2294@gmail.com>
2024-11-05 16:21:12 -0500

    increase epochs

--- model_train.py
+++ model_train.py
@@ -55,8 +55,8 @@ model = build_model(INPUT_IMG_SIZE, len(CLASSES))
 num_training_samples = training_gen.samples
 num_validation_samples = validation_gen.samples
 # short to make sure workflows are working
-n_epochs = 2
-# n_epochs = 10
+# n_epochs = 2
+n_epochs = 10
 # n_epochs_fine = 8
 
 steps_per_epoch = num_training_samples // BATCH_SIZE
