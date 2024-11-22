f6a3bd5441b25837a72c13e1107b2eb0ae7fd763
Ting Ting Chen Zeng <tin2294@gmail.com>
2024-11-22 02:15:41 -0500

    deploy_model fixes

--- deploy_model.py
+++ deploy_model.py
@@ -1,13 +1,17 @@
 import os
-from modelstore import ModelStore
+import tensorflow as tf
 from tensorflow.keras.models import load_model
 import glob
 
-model_file = glob.glob('/volume/model_v*')
+volume_dir = '/volume/'
+print(f"Contents of {volume_dir}:")
+print(os.listdir(volume_dir))
 
-if model_file:
+model_files = glob.glob(f'{volume_dir}model_v*.keras')
+
+if model_files:
+    model_file = model_files[0]
     print(f"Loading latest model from {model_file}")
-    model = tf.keras.models.load_model(model_file)
+    model = load_model(model_file)
 else:
     print("No model found.")
-
