f349cab6a0659011aff32b3616f8bdab45f9d323
Ting Ting Chen Zeng <tin2294@gmail.com>
2024-11-22 08:43:53 -0500

    fix path and stop training for now

--- deploy_model.py
+++ deploy_model.py
@@ -4,7 +4,7 @@ from tensorflow.keras.models import load_model
 import glob
 from modelstore import ModelStore
 
-model_dir = './model_storage/operatorai-model-store'
+model_dir = './model_storage'
 
 model_store = ModelStore.from_file_system(root_directory=model_dir)
 models = model_store.list_versions("image-classification")
