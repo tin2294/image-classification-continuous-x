3a8f51eba886b3b3aefe40f2b4ba64a5350444ca
Ting Ting Chen Zeng <tin2294@gmail.com>
2024-11-22 02:57:29 -0500

    avoid certain model keys

--- deploy_model.py
+++ deploy_model.py
@@ -15,6 +15,6 @@ model_files = glob.glob(f'{model_dir}model_v*.keras')
 if model_files:
     model_file = model_files[0]
     print(f"Loading latest model from {model_file}")
-    model = load_model(model_file)
+    model = load_model(model_file, compile=False)
 else:
     print("No model found.")
