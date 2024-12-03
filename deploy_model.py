b48f52b923c77cf0a8ea15429f8ec8b20df6a876
Ting Ting Chen Zeng <tin2294@gmail.com>
2024-12-03 11:40:07 -0500

    ssh key gen in gpu

--- deploy_model.py
+++ deploy_model.py
@@ -9,5 +9,19 @@ model_dir = './model_storage'
 model_store = ModelStore.from_file_system(root_directory=model_dir)
 models = model_store.list_versions("image-classification")
 
-for model in models:
-  print(model)
+if len(models) > 0:
+  latest_model_id = models[0]
+  print(f"Using the latest model with ID: {latest_model_id}")
+
+  clf = model_store.load(
+    domain="image-classification",
+    model_id=latest_model_id,
+  )
+
+  print(f"Model loaded with model store to: {clf}")
+
+  model = load_model(clf, compile=False)
+  print("LOADED MODEL WITH TF")
+
+else:
+  print("No models found.")
