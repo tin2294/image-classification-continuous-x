178a04790d7c752b223329a0e337db13a7d3ba31
Ting Ting Chen Zeng <tin2294@gmail.com>
2024-12-19 16:02:18 +0100

    lower threshold

--- evaluate_model.py
+++ evaluate_model.py
@@ -6,7 +6,7 @@ from utils import create_image_generator, plot_confusion_matrix
 MODEL_DIR = "/tmp/model_to_deploy"
 OUTPUT_FILE = "/tmp/temp_models/evaluation_metrics.txt"
 DEPLOY = "/tmp/temp_models/deploy.txt"
-ACCURACY_THRESHOLD = 0.9
+ACCURACY_THRESHOLD = 0.25
 CONFUSION_MATRIX_FILE = "/tmp/temp_models/confusion_matrix.pdf"
 EVALUATION_DIR = "/tmp/content/Food-11/evaluation"
 INPUT_IMG_SIZE = 112
